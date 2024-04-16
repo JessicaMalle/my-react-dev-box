b6d460ad52e53b94296a42d096102d08fb8a3b75
J. Malle <30345089+JessicaMalle@users.noreply.github.com>
2024-04-16 11:56:10 +0200

    add ui package dependency with react typescript

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@repo/ui/button";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,11 @@ function App() {
   return (
     <>
       <div>
+        <Button
+          label="Button"
+          onClick={() => {}}
+          size="large"
+        />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
